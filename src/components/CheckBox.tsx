42b7664b161dcb00c467425b2ac5e1a0d0e65580
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-17 23:39:23 +0600

    changeStatus

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,13 +1,27 @@
-export default function CheckBox({ todo }: { todo: Todo }) {
-  //   const changeStatus = (id: number) => {};
+import { trpc } from "~/utils/trpc";
 
+export default function CheckBox({ todo }: { todo: Todo }) {
+  const updateTodo = trpc.updateStatus.useMutation();
+
+  const { refetch } = trpc.todos.useQuery();
+
+  const changeStatus = (id: string) => {
+    updateTodo.mutate(
+      { id, isCompleted: !todo.isCompleted },
+      {
+        onSuccess: () => {
+          void refetch();
+        },
+      }
+    );
+  };
   return (
     <label className="relative inline-flex cursor-pointer items-center">
       <input
         type="checkbox"
         className="peer sr-only"
         checked={todo.isCompleted}
-        // onChange={() => changeStatus(todo.id)}
+        onChange={() => changeStatus(todo.id)}
       />
       <div className="peer h-6 w-11 rounded-full  bg-red-500 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none"></div>
       <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
