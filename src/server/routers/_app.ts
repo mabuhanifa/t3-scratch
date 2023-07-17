241f62d31aa8446f8afa460eafd7435690cd03a2
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-17 23:02:48 +0600

    addTodo procedure

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { prisma } from "../db";
 import { router, t } from "./trpc";
 
@@ -5,6 +6,13 @@ export const appRouter = router({
   todos: t.procedure.query(() => {
     return prisma.todo.findMany();
   }),
+  addTodo: t.procedure
+    .input(z.object({ title: z.string() }))
+    .mutation(({ input }) => {
+      return prisma.todo.create({
+        data: { title: input.title, isCompleted: false },
+      });
+    }),
 });
 
 export type AppRouter = typeof appRouter;
