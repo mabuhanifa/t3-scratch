59960ad637b372a32b225036ed7e3a4aab904519
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-17 17:36:57 +0600

    trpc setup

---
+++ src/utils/trpc.ts
@@ -0,0 +1,17 @@
+import { httpBatchLink } from "@trpc/client";
+import { createTRPCNext } from "@trpc/next";
+import type { AppRouter } from "../server/routers/_app";
+
+export const trpc = createTRPCNext<AppRouter>({
+  config() {
+    return {
+      links: [
+        httpBatchLink({
+          url: `http://localhost:3000/api/trpc`,
+        }),
+      ],
+    };
+  },
+
+  ssr: false,
+});
